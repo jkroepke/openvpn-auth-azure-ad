8440a478ed3fb233d214b5824c37660e76c8a268
Jan-Otto Kröpke <mail@jkroepke.de>
2023-03-26 19:35:17 +0200

    More strict CSP

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,11 +1,9 @@
-function copyToClipboard() {
-    navigator.clipboard.writeText(document.getElementById("code").innerText);
-}
-
 (function () {
     const code = location.search.match(/code=([A-Z0-9]+)/)
     if (code != null) {
         document.getElementById('code').innerText = code[1]
     }
+
+    document.getElementById('copy').onclick = () => navigator.clipboard.writeText(document.getElementById("code").innerText)
 })();
 
